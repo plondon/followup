00420f851ab6f9b36f88321f587120b5a32a9c61
plondon <philiplondon@Philips-MacBook-Air.local>
2013-12-13 16:44:05 -0500

    changed CGI to url_encode

--- followup.rb
+++ followup.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
-require 'CGI'
+require 'erb'
 require 'net/http'
 require 'uri'
 require 'json'
+include ERB::Util
 
 get '/' do
 	erb :search
@@ -10,7 +11,7 @@ end
 
 post '/' do
 	content_type :json
-	@query="http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+CGI::escape(params[:followup])+"&facet_field=day_of_week&begin_date=20120101&end_date=20131205&$pub_date&api-key=88192c1ed8fcf241f009ec9b0516d6ee:18:40073815"
+	@query="http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+url_encode(params[:followup])+"&facet_field=day_of_week&begin_date=20120101&end_date=20131205&$pub_date&api-key=88192c1ed8fcf241f009ec9b0516d6ee:18:40073815"
 
 	uri = URI.parse(@query)
 	response = Net::HTTP.get_response(uri)
